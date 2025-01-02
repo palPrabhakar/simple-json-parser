eee6464478646cc42fac37654042650d1a036e65
Prabhakar Pal <prabhakar.pal00@gmail.com>
2025-01-02 23:36:17 +0530

    added simple examples

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,47 @@
 int main(void) {
     std::cout << "Simple JSON Parser" << std::endl;
 
-    std::istringstream ss("{\"key \\\"1\":1}");
-    // std::istringstream ss("true");
-    // std::istringstream ss("null");
-    // std::istringstream ss("[true, null, 1]");
-    // std::istringstream ss("1.24");
-    // std::istringstream ss("\"lol\"");
+    {
+        std::istringstream ss("\"lol\"");
+        std::cout << "InputString: " << ss.str() << std::endl;
+        Parser parser(std::move(ss));
+        auto json = parser.Parse();
+    }
 
-    std::cout << "InputString: " << ss.str() << std::endl;
+    {
+        std::istringstream ss("1.24");
+        std::cout << "InputString: " << ss.str() << std::endl;
+        Parser parser(std::move(ss));
+        auto json = parser.Parse();
+    }
 
-    // std::istringstream ss("[1]");
-    Parser parser(std::move(ss));
-    parser.Parse();
+    {
+        std::istringstream ss("true");
+        std::cout << "InputString: " << ss.str() << std::endl;
+        Parser parser(std::move(ss));
+        auto json = parser.Parse();
+    }
+
+    {
+        std::istringstream ss("null");
+        std::cout << "InputString: " << ss.str() << std::endl;
+        Parser parser(std::move(ss));
+        auto json = parser.Parse();
+    }
+
+    {
+        std::istringstream ss("{\"key \\\"1\":1}");
+        std::cout << "InputString: " << ss.str() << std::endl;
+        Parser parser(std::move(ss));
+        auto json = parser.Parse();
+    }
+
+    {
+        std::istringstream ss("[true, null, 1]");
+        std::cout << "InputString: " << ss.str() << std::endl;
+        Parser parser(std::move(ss));
+        auto json = parser.Parse();
+    }
 
     return 0;
 }
